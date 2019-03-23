46e996f684a0bb6511405f9ee744c820891dd436
git1ahmad <ahmadninf@gmail.com>
2019-03-23 20:25:40 +0600

    upto add to cart

---
+++ src/main/java/com/emusicstore/dao/CartDao.java
@@ -0,0 +1,12 @@
+package com.emusicstore.dao;
+
+import com.emusicstore.model.Cart;
+
+public interface CartDao {
+
+    Cart getCartById(int cartId);
+
+    void update(Cart cart);
+
+
+}
---
+++ src/main/java/com/emusicstore/model/ShippingAddress.java
@@ -0,0 +1,108 @@
+package com.emusicstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+
+@Entity
+public class ShippingAddress implements Serializable {
+
+    public static final long serialVersionUID = -5700618116947296378L;
+
+
+    @Id
+    @GeneratedValue
+    private int shippngAddressId;
+
+    private String streetName;
+    private String apartmentNumber;
+
+    private String city;
+    private String state;
+
+    private String country;
+
+    private String zipcode;
+
+    @OneToOne
+    private Customer customer;
+
+
+    public int getShippngAddressId() {
+        return shippngAddressId;
+    }
+
+    public void setShippngAddressId(int shippngAddressId) {
+        this.shippngAddressId = shippngAddressId;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "ShippingAddress{" +
+                "streetName='" + streetName + '\'' +
+                ", apartmentNumber='" + apartmentNumber + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", zipCode='" + zipcode + '\'' +
+                '}';
+    }
+}
